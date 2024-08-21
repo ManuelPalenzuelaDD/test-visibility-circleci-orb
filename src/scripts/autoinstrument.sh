91a5c2238c1d3dc58e7a428e12ca3a182a19e994
Manuel Palenzuela Merino <manuel.palenzuela@datadoghq.com>
2024-08-21 11:14:55 +0200

    copyright

--- src/scripts/autoinstrument.sh
+++ src/scripts/autoinstrument.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the Apache License Version 2.0.
+# This product includes software developed at Datadog (https://www.datadoghq.com/)
+# Copyright 2024-present Datadog, Inc.
+
 mkdir .datadog
 
 # Propagate service name and API key from inputs to environment variables
