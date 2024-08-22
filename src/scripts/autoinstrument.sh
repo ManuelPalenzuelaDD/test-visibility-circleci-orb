4dc1c99859e36e51da2332d9bb9fa24734156bc6
Manuel Palenzuela Merino <manuel.palenzuela@datadoghq.com>
2024-08-22 10:00:40 +0200

    test

--- src/scripts/autoinstrument.sh
+++ src/scripts/autoinstrument.sh
@@ -48,7 +48,7 @@ chmod +x ./install_test_visibility.sh
 
 while IFS='=' read -r name value; do
   if [[ $name =~ ^[A-Za-z_][A-Za-z0-9_]*$ ]]; then
-    echo "export $name=$value" >> "$BASH_ENV"
+    echo "export $name=\"$value\"" >> "$BASH_ENV"
   fi
 done < <(./install_test_visibility.sh)
 
